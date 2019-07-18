54b4650e44f4c1b405a75b6e0a8236dce30b3660
songzhw <songzhw2012@gmail.com>
2019-07-18 16:37:33 -0400

    add the support of landscape

--- sha3/src/ui/HomeScreen.tsx
+++ sha3/src/ui/HomeScreen.tsx
@@ -1,8 +1,9 @@
-import React, { useState } from "react";
-import { View, ViewProps, Text, StyleSheet } from "react-native";
+import React, { useEffect, useState } from "react";
+import { View, ViewProps, Text, StyleSheet, SafeAreaView } from "react-native";
 import { Button, Icon, Radio } from "beeshell";
 import { NavigationScreenProps } from "react-navigation";
 import { GAME_MODEL_4, GAME_MODEL_6, GAME_MODEL_8 } from "../data/Const";
+import Orientation from "react-native-orientation";
 
 interface IProps extends ViewProps, NavigationScreenProps {
 }
@@ -13,22 +14,26 @@ interface IProps extends ViewProps, NavigationScreenProps {
 export const HomeScreen = (props: IProps) => {
   const [gameMode, setGameMode] = useState(GAME_MODEL_4);
 
+  useEffect(() => {
+    Orientation.lockToLandscape();
+  }, []);
+
   function onStart() {
     console.log(`szw onstart()`);
     props.navigation.navigate("GameScreen");
   }
 
   return (
-    <View>
+    <SafeAreaView>
       <Radio value={gameMode} onChange={(mode: number) => setGameMode(mode)}
-        >
+      >
         <Radio.Item label={"四人局(1主公, 1忠臣, 1反贼, 1内奸"} value={GAME_MODEL_4}/>
         <Radio.Item label={"六人局(1主公, 1忠臣, 2反贼, 2内奸"} value={GAME_MODEL_6}/>
         <Radio.Item label={"八人局(1主公, 2忠臣, 3反贼, 2内奸"} value={GAME_MODEL_8}/>
       </Radio>
 
       <Button type="primary" size="md" onPress={onStart}> Start </Button>
-    </View>
+    </SafeAreaView>
   );
 };
 
