ad7c91fd1132fdc82f7815b40170ca265c3b3a67
songzhw <songzhw2012@gmail.com>
2019-07-19 11:11:27 -0400

    add card deck

--- sha3/src/data/CardDeck.ts
+++ sha3/src/data/CardDeck.ts
@@ -8,10 +8,68 @@ export function compareCardRank(one: ICardRank, two: ICardRank) {
   return idx1 > idx2 ? 1 : (idx1 == idx2 ? 0 : -1);
 }
 
+//TODO expand
+type ICardLabel = "杀" | "闪";
+
 
 interface ICard {
   suit: "spade" | "heart" | "diamond" | "club";  //黑桃|红心|方块|梅花
+  rank: ICardRank;
+  label: ICardLabel;
 }
 
 
-export const CardDeck = [];
+export const CardDeck: Array<ICard> = [
+  { suit: "spade", rank: "1", label: "杀" },
+  { suit: "spade", rank: "2", label: "杀" },
+  { suit: "spade", rank: "3", label: "杀" },
+  { suit: "spade", rank: "4", label: "杀" },
+  { suit: "spade", rank: "5", label: "杀" },
+  { suit: "spade", rank: "6", label: "杀" },
+  { suit: "spade", rank: "7", label: "杀" },
+  { suit: "spade", rank: "8", label: "杀" },
+  { suit: "spade", rank: "9", label: "杀" },
+  { suit: "spade", rank: "A", label: "杀" },
+  { suit: "heart", rank: "J", label: "杀" },
+  { suit: "heart", rank: "1", label: "杀" },
+  { suit: "heart", rank: "2", label: "杀" },
+  { suit: "heart", rank: "3", label: "杀" },
+  { suit: "heart", rank: "4", label: "杀" },
+  { suit: "heart", rank: "5", label: "杀" },
+  { suit: "heart", rank: "6", label: "杀" },
+  { suit: "heart", rank: "7", label: "杀" },
+  { suit: "heart", rank: "8", label: "杀" },
+  { suit: "heart", rank: "9", label: "杀" },
+  { suit: "heart", rank: "K", label: "杀" },
+  { suit: "heart", rank: "J", label: "杀" },
+  { suit: "diamond", rank: "4", label: "杀" },
+  { suit: "diamond", rank: "5", label: "杀" },
+  { suit: "diamond", rank: "6", label: "杀" },
+  { suit: "diamond", rank: "7", label: "杀" },
+  { suit: "diamond", rank: "8", label: "杀" },
+  { suit: "diamond", rank: "9", label: "杀" },
+  { suit: "diamond", rank: "K", label: "杀" },
+  { suit: "diamond", rank: "3", label: "杀" },
+  { suit: "diamond", rank: "2", label: "闪" },
+  { suit: "diamond", rank: "5", label: "闪" },
+  { suit: "diamond", rank: "6", label: "闪" },
+  { suit: "diamond", rank: "7", label: "闪" },
+  { suit: "diamond", rank: "2", label: "闪" },
+  { suit: "diamond", rank: "Q", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" },
+  { suit: "club", rank: "J", label: "闪" }
+];
