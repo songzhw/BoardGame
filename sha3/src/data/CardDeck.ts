262f49333fab49a0cdbdbb5d602a868760d79bf9
songzhw <songzhw2012@gmail.com>
2019-07-19 10:58:13 -0400

    js/ts - comparable

--- sha3/src/data/CardDeck.ts
+++ sha3/src/data/CardDeck.ts
@@ -1,3 +1,12 @@
+const CardRankList = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "J", "Q", "K", "A"];
+type ICardRank = "1" | "2" | "3" | "4" | "5" | "6" | "7" | "8" | "9" | "10" | "J" | "Q" | "K" | "A";
+
+/** @return : first < second, 返回-1;  first > second, 返回1; 相等则返回0 */
+export function compareCardRank(one: ICardRank, two: ICardRank) {
+  const idx1 = CardRankList.findIndex(value => value === one);
+  const idx2 = CardRankList.findIndex(value => value === two);
+  return idx1 > idx2 ? 1 : (idx1 == idx2 ? 0 : -1);
+}
 
 
 interface ICard {
